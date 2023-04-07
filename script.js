13a33652c5842c23b5940f535e8475e7928107b4
Юрковец Константин <wolf13z@mail.ru>
2023-04-07 16:06:03 +0300

    accordion in work

--- script.js
+++ script.js
@@ -324,6 +324,63 @@ const choices = new Choices(element, {
 
 
 
+// ACCORDEON
+(() => {
+  new Accordion(".js-accordion-container", {
+    openOnInit: [0]
+  });
+})();
+
+
+// Табы
+const params = {
+  wrap: "js-tabs-wrap",
+  content: "js-tab-content",
+  active: "js-active",
+  tabsClass: "js-tab-btn"
+};
+
+function setTabs(params) {
+  const tabBtns = document.querySelectorAll(`.${params.tabsClass}`);
+
+  function onTabClick(e) {
+    e.preventDefault();
+    const path = this.dataset.path;
+    const wrap = this.closest(`.${params.wrap}`);
+    const currentContent = wrap.querySelector(`.${params.content}[data-target="${path}"]`);
+    const contents = wrap.querySelectorAll(`.${params.content}`);
+
+    contents.forEach((el) => {
+      el.classList.remove(params.active);
+    });
+
+    currentContent.classList.add(params.active);
+
+    tabBtns.forEach((el) => {
+      el.classList.remove(params.active);
+    });
+
+    this.classList.add(params.active);
+  }
+
+  tabBtns.forEach(function (el) {
+    el.addEventListener("click", onTabClick);
+  });
+}
+
+setTabs(params);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // TOOLTIP
